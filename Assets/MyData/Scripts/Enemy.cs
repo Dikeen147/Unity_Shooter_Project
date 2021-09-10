352e3593796dc0a9fd74ef85c0e7a772cf0756ce
Konstantin <ConstAnto147@yandex.ru>
2021-09-11 02:01:19 +0300

    added nav mesh agent, bake location

--- Assets/MyData/Scripts/Enemy.cs
+++ Assets/MyData/Scripts/Enemy.cs
@@ -1,10 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
     [SerializeField] private int _health;
+    [SerializeField] private NavMeshAgent _agent;
+    [SerializeField] private Transform[] _enemyWalkPoints;
+
+    int _currentEnemyWalkPoint;
+
+    private void Awake()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Start()
+    {
+        //_agent.SetDestination(_target.position);    
+    }
+
+    private void FixedUpdate()
+    {
+        if (_agent.remainingDistance < _agent.stoppingDistance)
+        {
+            _currentEnemyWalkPoint = (_currentEnemyWalkPoint + 1) % _enemyWalkPoints.Length;
+            _agent.SetDestination(_enemyWalkPoints[_currentEnemyWalkPoint].position);
+        }
+    }
 
     public void Hurt(int damage)
     {
